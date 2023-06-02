9de924a50bd74ea7a04a904e3d4c3f6b7c283345
Snezhana Makhova <makhova-s94@mail.ru>
2023-06-02 21:33:03 +0300

    added docstring to str_func.py and another function

--- str_func.py
+++ str_func.py
@@ -1,5 +1,14 @@
 def shows_word_upper(string):
+    """возвращает строку заглавными буквами"""
     return string.upper()
 
 #my_string = 'hello'
 #print(shows_word_upper(my_string))
+
+
+def shows_word_title(string):
+    """возвращает строку с заглавной буквы"""
+    return string.title()
+
+#my_string = 'hello'
+#print(shows_word_title(my_string))
